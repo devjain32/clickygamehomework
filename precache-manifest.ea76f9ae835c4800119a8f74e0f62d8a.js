27a1452f67e45a637989457ef907470cd66a6fc6
Dev Jain <devrjain32200@gmail.com>
2019-07-30 23:12:39 -0500

    Updates

---
+++ precache-manifest.ea76f9ae835c4800119a8f74e0f62d8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8721b9550b88cf1744dda15d200d64ba",
+    "url": "/clickygamehomework/index.html"
+  },
+  {
+    "revision": "c963348ae6b4dda301cd",
+    "url": "/clickygamehomework/static/css/main.e072bd8d.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickygamehomework/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c963348ae6b4dda301cd",
+    "url": "/clickygamehomework/static/js/main.65463972.chunk.js"
+  },
+  {
+    "revision": "a8b93e568cd1cef05d25",
+    "url": "/clickygamehomework/static/js/runtime~main.b5711d9c.js"
+  }
+]);
